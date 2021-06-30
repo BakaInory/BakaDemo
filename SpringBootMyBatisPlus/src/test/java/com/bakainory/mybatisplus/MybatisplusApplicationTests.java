254e1d0962f2a2faf3c826709e5a7ea76fbc43e7
BakaInory <bakainory@gmail.com>
2021-06-30 10:27:46 +0800

    MyBatisPlus整合Demo框架

---
+++ SpringBootMyBatisPlus/src/test/java/com/bakainory/mybatisplus/MybatisplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakainory.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
